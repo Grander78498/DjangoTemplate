62059bf21ff4da24baaa21e478cfef4d4acca3b8
Grander78498 <grander.l.l.1242@gmail.com>
2024-11-06 19:04:47 +0300

    Работает связка с тритоном

--- project/django_video/settings.py
+++ project/django_video/settings.py
@@ -126,7 +126,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_ROOT = BASE_DIR.parent / 'staticfiles'
 MEDIA_URL = 'media/'
 MEDIA_ROOT = BASE_DIR.parent / 'mediafiles'
 CSRF_TRUSTED_ORIGINS = [f"http://{host}:1337" for host in os.environ.get("DJANGO_ALLOWED_HOSTS").split(" ")]
--- project/video_app/views.py
+++ project/video_app/views.py
@@ -6,6 +6,7 @@ from django.urls import reverse, reverse_lazy
 from django.http import HttpRequest, HttpResponseRedirect, HttpResponse
 from .models import FileUpload
 from .forms import UploadFileForm
+from . import utils
 
 
 def root_page(request: HttpRequest):
@@ -19,7 +20,11 @@ def main_page(request):
         print(request.POST, request.FILES)
         form = UploadFileForm(request.POST, request.FILES)
         if form.is_valid():
-            FileUpload.objects.create(file=request.FILES['file'])
+            file = FileUpload.objects.create(file=request.FILES['file'])
+            file_path = file.file.path
+            client = utils.get_client()
+            result = utils.predict_model(file_path, client)
+            print(result)
             return HttpResponse('Fine')
     else:
         form = UploadFileForm()
